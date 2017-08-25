787e9ba3f9211b1381f77b79abb001143e3acea3
Felipe Chiarotti <felipechi97@gmail.com>
2017-08-25 09:16:21 -0300

    Update Compilator

---
+++ src/parser/stack.c
@@ -0,0 +1,19 @@
+struct Stack{
+    int data[100];
+    int size;
+};
+
+void stack_up(struct Stack stack, int value){
+    stack.data[stack.size] = value;
+    stack.size++;
+}
+
+int stack_down(struct Stack stack){
+    int valor = stack.data[stack.size];
+    stack.data[stack.size] = 0;
+    return valor;
+}
+
+int stack_top(struct Stack stack){
+    return stack.data[stack.size];
+}
